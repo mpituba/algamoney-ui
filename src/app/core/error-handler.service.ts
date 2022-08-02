476f544d7130feb9985d5ed96ca51ab23cce1631
Marcelo Pituba <mpituba@yahoo.com>
2022-08-02 15:05:44 -0300

    17.14. Desafio:Mensagem erro na exclusão de pessoa

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
@@ -8,7 +9,7 @@ export class ErrorHandlerService {
 
   constructor(private messageService: MessageService) {
 
-   }
+  }
 
   handle(errorResponse: any) {
     let msg: string;
@@ -16,9 +17,26 @@ export class ErrorHandlerService {
     if(typeof errorResponse === 'string') {
       msg = errorResponse;
 
-    } else {
-      msg = 'Erro ao processar serviço remoto. Tente novamente.';
-      console.error('Ocorreu um erro', errorResponse);
+    } else if ( errorResponse instanceof HttpErrorResponse &&
+                (errorResponse.status >=400 &&
+                  errorResponse.status <= 499) ) {
+
+
+        msg = 'Ocorreu um erro ao processar a sua solicitação.';
+
+        try {
+         msg =  errorResponse.error[0].mensagemUsuario;
+
+
+        } catch (e) {
+        }
+
+        console.error('Ocorreu um erro', errorResponse);
+
+      } else {
+        msg = 'Erro ao processar serviço remoto. Tente novamente.';
+        console.error('Ocorreu um erro', errorResponse);
+
     }
 
     this.messageService.add({ severity: 'error', detail: msg });
