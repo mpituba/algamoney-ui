ce76f4462b662fc5b6caef38b91ba6f186f73594
Marcelo Pituba <mpituba@yahoo.com>
2022-08-11 12:47:21 -0300

    18.02. Configurando rotas na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { CoreModule } from './core/core.module';
 
+const routes: Routes = [
+  { path: 'lancamentos', component: LancamentosPesquisaComponent },
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
+  { path:'pessoas', component: PessoasPesquisaComponent  }
+];
 
 @NgModule({
   declarations: [
@@ -21,6 +29,7 @@ import { CoreModule } from './core/core.module';
     BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
+    RouterModule.forRoot(routes),
 
     LancamentosModule,
     PessoasModule,
