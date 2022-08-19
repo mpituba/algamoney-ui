fd819ea4d3ee503726615d6f1220942da3d672a5
Marcelo Pituba <mpituba@yahoo.com>
2022-08-19 14:26:55 -0300

    18.13. Refatorando rotas para usar Routing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,15 +14,7 @@ import { PessoasModule } from './pessoas/pessoas.module';
 import { CoreModule } from './core/core.module';
 import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-  { path: 'lancamentos', component: LancamentosPesquisaComponent },
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
-  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
-  { path:'pessoas', component: PessoasPesquisaComponent  },
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
-  { path: '**', redirectTo: 'pagina-nao-encontrada' }
-];
+
 
 @NgModule({
   declarations: [
@@ -34,11 +26,12 @@ const routes: Routes = [
     BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes),
+
 
     LancamentosModule,
     PessoasModule,
-    CoreModule
+    CoreModule,
+    AppRoutingModule
   ],
   providers: [],
 
