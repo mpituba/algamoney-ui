19142f4ecc414bab9d81af86e490ef79865f053c
Marcelo Pituba <mpituba@yahoo.com>
2022-09-13 17:15:35 -0300

    20.03. Respondendo requisições Node.js e Express Ambiente de produção configurado como localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mpitubamoney-api.herokuapp.com/',
-  tokenAllowedDomains: [ /mpitubamoney-api.herokuapp.com/ ],
+  apiUrl: 'http://localhost:8080',
+  tokenAllowedDomains: [ /localhost:8080/ ],
+
   tokenDisallowedRoutes: [/\/oauth\/token/],
 };
