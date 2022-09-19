58c9c288c18555a9f9a81d801c16fef5b6311fe2
Marcelo Pituba <mpituba@yahoo.com>
2022-09-19 15:29:11 -0300

    20.04. Código rodando em produção no Heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
-  tokenAllowedDomains: [ /localhost:8080/ ],
-  tokenDisallowedRoutes: [ /\/oauth\/token/],
+  tokenAllowedDomains: [/localhost:8080/],
+  tokenDisallowedRoutes: [/\/oauth\/token/],
 };
 
 /*
